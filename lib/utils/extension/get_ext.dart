ea179a28384023baf9b5c9bdbf3d7ae783b7dd1f
OneMiOne <106773566+OneMiOne@users.noreply.github.com>
2026-01-27 14:53:25 +0800

    Feng

--- lib/utils/extension/get_ext.dart
+++ lib/utils/extension/get_ext.dart
@@ -4,3 +4,44 @@ extension GetExt on GetInterface {
   S putOrFind<S>(InstanceBuilderCallback<S> dep, {String? tag}) =>
       GetInstance().putOrFind(dep, tag: tag);
 }
+
+extension TransitionExt on Transition {
+  String get label {
+    switch (this) {
+      case Transition.fade:
+        return '淡入淡出（Fade）';
+      case Transition.rightToLeft:
+        return '左滑（Right To Left）';
+      case Transition.leftToRight:
+        return '右滑（Left To Right）';
+      case Transition.topToBottom:
+        return '下滑（Top To Bottom）';
+      case Transition.bottomToTop:
+        return '上滑（Bottom To Top）';
+      case Transition.noTransition:
+        return '无动画（No Transition）';
+      case Transition.rightToLeftWithFade:
+        return '渐变左滑（Right To Left With Fade）';
+      case Transition.leftToRightWithFade:
+        return '渐变右滑（Left To Right With Fade）';
+      case Transition.cupertino:
+        return 'Apple（Cupertino）';
+      case Transition.size:
+        return '扩展（Size）';
+      case Transition.zoom:
+        return '缩放（Zoom）';
+      case Transition.native:
+        return '系统（Native）';
+      case Transition.circularReveal:
+        return '圆展（Circular Reveal）';
+      case Transition.fadeIn:
+        return '淡入（Fade In）';
+      case Transition.downToUp:
+        return '上展（Down To Up）';
+      case Transition.upToDown:
+        return '下展（Up To Down）';
+      default:
+        return name;
+    }
+  }
+}
